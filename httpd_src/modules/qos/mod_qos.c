650b839e744b22dd04642755a7af4442b0082ace
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-05-21 20:10:38 +0000

    customizable error page
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@9 49bada20-169a-462c-a56a-03074c89f563

--- httpd_src/modules/qos/mod_qos.c
+++ httpd_src/modules/qos/mod_qos.c
@@ -6,7 +6,7 @@
  *
  * The Apache Web Servers requires threads and processes to serve
  * requests. Each TCP connection to the web server occupies one
- * thread or processes. Sometimes, a server gets too busy to serve
+ * thread or process. Sometimes, a server gets too busy to serve
  * every request due the lack of free processes or threads.
  *
  * This module implements control mechanisms that can provide
@@ -15,8 +15,8 @@
  * This release features the following features:
  * - Limits the number of concurrent requests for a location. The
  *   implementation uses the scoreboard to determine the number
- *   of concurrent requests per location. This requires many
- *   strncmp() calls.
+ *   of concurrent requests per location.
+ * - Customizable error page for denied requests.
  *
  * See http://sourceforge.net/projects/mod-qos/ for further
  * details.
@@ -44,7 +44,7 @@
  * Version
  ***********************************************************************/
 
-static const char rcsid[] = "$Header: /home/cvs/m/mo/mod-qos/src/httpd_src/modules/qos/mod_qos.c,v 1.2 2007-05-19 07:56:34 pbuchbinder Exp $";
+static const char rcsid[] = "$Header: /home/cvs/m/mo/mod-qos/src/httpd_src/modules/qos/mod_qos.c,v 1.3 2007-05-21 20:10:38 pbuchbinder Exp $";
 
 /************************************************************************
  * Includes
@@ -79,6 +79,7 @@ static const char rcsid[] = "$Header: /home/cvs/m/mo/mod-qos/src/httpd_src/modul
 typedef struct {
   apr_table_t *location_t;
   int default_loc_limit;
+  const char *error_page;
 } qos_srv_config;
 
 /************************************************************************
@@ -91,6 +92,25 @@ int server_limit, thread_limit;
 /************************************************************************
  * private functions
  ***********************************************************************/
+
+/**
+ * Returns custom error page
+ */
+static void qos_error_response(request_rec *r, const char *error_page) {
+  /* do (almost) the same as ap_die() does */
+  const char *error_notes;
+  r->status = HTTP_INTERNAL_SERVER_ERROR;
+  r->connection->keepalive = AP_CONN_CLOSE;
+  r->no_local_copy = 1;
+  apr_table_setn(r->subprocess_env, "REQUEST_METHOD", r->method);
+  if ((error_notes = apr_table_get(r->notes, 
+                                   "error-notes")) != NULL) {
+    apr_table_setn(r->subprocess_env, "ERROR_NOTES", error_notes);
+  }
+  r->method = apr_pstrdup(r->pool, "GET");
+  r->method_number = M_GET;
+  ap_internal_redirect(error_page, r);
+}
 
 /************************************************************************
  * handlers
@@ -109,7 +129,7 @@ static int qos_header_parser(request_rec * r) {
 
     /* get the request limitation for this location */
     int limit = sconf->default_loc_limit;
-    char *limit_location = apr_pstrdup(r->pool, "/");
+    const char *limit_location = apr_pstrdup(r->pool, "/");
     int limit_location_len = strlen(limit_location);
     apr_table_entry_t *entry = (apr_table_entry_t *)apr_table_elts(sconf->location_t)->elts;
     for(i = 0; i < apr_table_elts(sconf->location_t)->nelts; i++) {
@@ -152,7 +172,11 @@ static int qos_header_parser(request_rec * r) {
         ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, r,
                       QOS_LOG_PFX"access denied, rule: %s(%d), concurrent requests: %d",
                       limit_location, limit, current);
-        return HTTP_INTERNAL_SERVER_ERROR;;
+        if(sconf->error_page) {
+          qos_error_response(r, sconf->error_page);
+          return DONE;
+        } 
+        return HTTP_INTERNAL_SERVER_ERROR;
       }
     }
   }
@@ -163,7 +187,7 @@ static int qos_header_parser(request_rec * r) {
  * Intit the server configuration
  */
 static int qos_post_config(apr_pool_t *pconf, apr_pool_t *plog, apr_pool_t *ptemp, server_rec *bs) {
-  char *rev = apr_pstrdup(ptemp, "$Revision: 1.2 $");
+  char *rev = apr_pstrdup(ptemp, "$Revision: 1.3 $");
   char *e = strrchr(rev, ' ');
   e[0] = '\0';
   rev++;
@@ -185,6 +209,7 @@ static void *qos_srv_config_create(apr_pool_t * p, server_rec *s) {
   qos_srv_config *sconf=(qos_srv_config *)apr_pcalloc(p, sizeof(qos_srv_config));
   sconf->location_t = apr_table_make(p, 2);
   sconf->default_loc_limit = 0; /** no limitation */
+  sconf->error_page = NULL;
   return sconf;
 }
 
@@ -219,16 +244,33 @@ const char *qos_loc_con_def_cmd(cmd_parms * cmd, void *dcfg, const char *limit) 
   return NULL;
 }
 
+/**
+ * Defines custom error page
+ */
+const char *qos_error_page_cmd(cmd_parms * cmd, void *dcfg, const char *path) {
+  qos_srv_config* sconf = (qos_srv_config*)ap_get_module_config(cmd->server->module_config,
+                                                                &qos_module);
+  sconf->error_page = apr_pstrdup(cmd->pool, path);
+  if(sconf->error_page[0] != '/') {
+    return apr_psprintf(cmd->pool, "%s: requires absolute path (%s)", 
+                        cmd->directive->directive, sconf->error_page);
+  }
+  return NULL;
+}
+
 static const command_rec qos_config_cmds[] = {
   AP_INIT_TAKE2("QS_LocRequestLimit", qos_loc_con_cmd, NULL,
-               RSRC_CONF,
+                RSRC_CONF,
                 "QS_LocRequestLimit <location> <number>, defines the number of"
                 " concurrent requests to the location. Default is defined by the"
                 " QS_LocRequestLimitDefault directive."),
   AP_INIT_TAKE1("QS_LocRequestLimitDefault", qos_loc_con_def_cmd, NULL,
-               RSRC_CONF,
+                RSRC_CONF,
                 "QS_LocRequestLimitDefault <number>, defines the default for the"
                 " QS_LocRequestLimit directive."),
+  AP_INIT_TAKE1("QS_ErrorPage", qos_error_page_cmd, NULL,
+                RSRC_CONF,
+                "QS_ErrorPage <url>, defines a custom error page."),
   NULL,
 };
 
