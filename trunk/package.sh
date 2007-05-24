feb00e8ecdec360b21427d11530b977a8268d13a
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-05-24 20:15:41 +0000

    prepare release 1.3

--- trunk/package.sh
+++ trunk/package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Header: /home/cvs/m/mo/mod-qos/src/package.sh,v 1.1 2007-05-20 12:52:50 pbuchbinder Exp $
+# $Header: /home/cvs/m/mo/mod-qos/src/package.sh,v 1.2 2007-05-24 20:15:40 pbuchbinder Exp $
 #
 # Script to build file release
 #
@@ -11,6 +11,26 @@
 #
 
 TOP=`pwd`
+VERSION=`grep mod_qos.c,v httpd_src/modules/qos/mod_qos.c | awk '{print $8}'`
+echo "build version $VERSION"
+
+rm -rf mod_qos-${VERSION}*
+mkdir -p mod_qos-${VERSION}/doc
+mkdir -p mod_qos-${VERSION}/apache2
+
+echo "install documentation"
+cp doc/README.TXT mod_qos-${VERSION}
+cp doc/index.html mod_qos-${VERSION}/doc
+cp doc/mod_qos_s.gif mod_qos-${VERSION}/doc
+
+echo "install source"
+cp httpd_src/modules/qos/mod_qos.c mod_qos-${VERSION}/apache2
+cp httpd_src/modules/qos/config.m4 mod_qos-${VERSION}/apache2
+cp httpd_src/modules/qos/Makefile.in mod_qos-${VERSION}/apache2
+
+echo "package"
+tar cf mod_qos-${VERSION}.tar mod_qos-${VERSION}
+gzip mod_qos-${VERSION}.tar
+rm -r mod_qos-${VERSION}
 
 echo "END"
-
