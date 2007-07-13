c083e7d9cd7e5c0c35318e2944700fd445720298
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-07-13 09:28:27 +0000

    new implementation with improved performance

--- trunk/package.sh
+++ trunk/package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Header: /home/cvs/m/mo/mod-qos/src/package.sh,v 1.3 2007-07-05 17:07:13 pbuchbinder Exp $
+# $Header: /home/cvs/m/mo/mod-qos/src/package.sh,v 1.4 2007-07-13 09:28:27 pbuchbinder Exp $
 #
 # Script to build file release
 #
@@ -8,6 +8,8 @@
 # contains the index.html/readme about mod_qos
 # ./apache
 # contains the source code
+# ./tools
+# supplemental code
 #
 
 TOP=`pwd`
@@ -17,9 +19,11 @@ echo "build version $VERSION"
 rm -rf mod_qos-${VERSION}*
 mkdir -p mod_qos-${VERSION}/doc
 mkdir -p mod_qos-${VERSION}/apache2
+mkdir -p mod_qos-${VERSION}/tools
 
 echo "install documentation"
 cp doc/README.TXT mod_qos-${VERSION}
+cp doc/LICENSE.txt mod_qos-${VERSION}/doc
 cp doc/index.html mod_qos-${VERSION}/doc
 cp doc/mod_qos_s.gif mod_qos-${VERSION}/doc
 cp doc/favicon.ico mod_qos-${VERSION}/doc
@@ -28,6 +32,13 @@ echo "install source"
 cp httpd_src/modules/qos/mod_qos.c mod_qos-${VERSION}/apache2
 cp httpd_src/modules/qos/config.m4 mod_qos-${VERSION}/apache2
 cp httpd_src/modules/qos/Makefile.in mod_qos-${VERSION}/apache2
+
+echo "tools"
+cp tools/qs_util.h mod_qos-${VERSION}/tools
+cp tools/qs_util.c mod_qos-${VERSION}/tools
+cp tools/qslog.c mod_qos-${VERSION}/tools
+cp tools/qscheck.c mod_qos-${VERSION}/tools
+cp tools/Makefile mod_qos-${VERSION}/tools
 
 echo "package"
 tar cf mod_qos-${VERSION}.tar mod_qos-${VERSION}
