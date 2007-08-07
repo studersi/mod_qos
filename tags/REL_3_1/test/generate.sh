b15902536755d09236e32db9c4da1fa0cfc3a6b4
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2007-08-07 20:53:59 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_3_1'.

---
+++ tags/REL_3_1/test/generate.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+ROOT=`pwd`
+QS_UID=`id`
+QS_UID_STR=`expr "$QS_UID" : 'uid=[0-9]*.\([a-z,A-Z,0-9,_]*\)'`
+QS_UID=`id`
+QS_GID=`expr "$QS_UID" : '.*gid=[0-9]*.\([a-z,A-Z,0-9,_]*\)'`
+QS_UID=`id`
+QS_UID=`expr "$QS_UID" : 'uid=\([0-9]*\)'`
+QS_PORT_BASE=`expr ${QS_UID} - 1000`
+QS_PORT_BASE=`expr $QS_PORT_BASE '*' 120`
+QS_PORT_BASE=`expr $QS_PORT_BASE + 5000`
+QS_PORT_BASE1=`expr $QS_PORT_BASE + 1`
+QS_PORT_BASE2=`expr $QS_PORT_BASE + 2`
+QS_PORT_BASE5=`expr $QS_PORT_BASE + 5`
+
+echo "QS_PORT_BASE=$QS_PORT_BASE"   >  HOSTS_AND_PORTS
+echo "QS_PORT_BASE1=$QS_PORT_BASE1" >> HOSTS_AND_PORTS
+echo "QS_PORT_BASE2=$QS_PORT_BASE2" >> HOSTS_AND_PORTS
+echo "QS_PORT_BASE5=$QS_PORT_BASE5" >> HOSTS_AND_PORTS
+
+sed <conf/httpd.conf.tmpl >conf/httpd.conf \
+    -e "s;##ROOT##;$ROOT;g" \
+    -e "s;##USR##;$QS_UID_STR;g" \
+    -e "s;##QS_PORT_BASE##;$QS_PORT_BASE;g" \
+    -e "s;##QS_PORT_BASE1##;$QS_PORT_BASE1;g" \
+    -e "s;##QS_PORT_BASE2##;$QS_PORT_BASE2;g" \
+    -e "s;##QS_PORT_BASE5##;$QS_PORT_BASE5;g"
+
+sed <appl_conf/httpd.conf.tmpl >appl_conf/httpd.conf \
+    -e "s;##ROOT##;$ROOT;g" \
+    -e "s;##USR##;$QS_UID_STR;g" \
+    -e "s;##QS_PORT_BASE##;$QS_PORT_BASE;g" \
+    -e "s;##QS_PORT_BASE1##;$QS_PORT_BASE1;g" \
+    -e "s;##QS_PORT_BASE2##;$QS_PORT_BASE2;g" \
+    -e "s;##QS_PORT_BASE5##;$QS_PORT_BASE5;g"
+
+if [ ! -d logs ]; then
+    mkdir logs
+fi
+
+
