c1ca1cf2065039bae325b1608e6c84417e9dd21b
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-09-21 20:01:33 +0000

    *** empty log message ***

--- trunk/dist/bin.sh
+++ trunk/dist/bin.sh
@@ -3,6 +3,6 @@
 rm -f mod_qos_bin.tar.gz
 
 cd $1
-tar cvf ../mod_qos_bin.tar --owner root --group bin .
-gzip ../mod_qos_bin.tar
+tar cvf ../mod_qos-${1}-bin.tar --owner root --group bin .
+gzip ../mod_qos-${1}-bin.tar
 
