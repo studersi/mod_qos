a598d89908f9c2cdd9b66be7f1639486e168f813
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-09-27 14:30:19 +0000

    *** empty log message ***

--- trunk/tools/filter/filter.sh
+++ trunk/tools/filter/filter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-echo -e "/webapp/test\n/webapp/mo^\"'re?name=value\n/webapp/sp;v1.1%20?show_all" | ./qsfilter $@
+echo -e "/app/test\n/other\n/m/this?name=val\n/app/mo^\"'re?name=value\n/app/sp;v1.1%20?show_all\n/app/f/main.do?show=7834639&infoWord=Yes+Go&refN=98876" | ./qsfilter $@
 
