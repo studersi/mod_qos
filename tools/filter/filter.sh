cf896af963db1e7c9a3fbc104c200fffb3469ae8
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-09-28 20:49:52 +0000

    base64 replacement
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@162 49bada20-169a-462c-a56a-03074c89f563

--- tools/filter/filter.sh
+++ tools/filter/filter.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 
-echo -e "/app/test\n/other\n/m/this?name=val\n/app/mo^\"'re?name=value\n/app/sp;v1.1%20?show_all\n/app/f/main.do?show=7834639&infoWord=Yes+Go&refN=98876&code=rk1lhTLn266YBx9$$\n/app/sp2/mo.re?name=+\$m?ore&n=b" | ./qsfilter $@
-
-
+echo -e "/app/test\n/other\n/m/this?name=val\n/app/mo^\"'re?name=value\n/app/sp;v1.1%20?show_all\n/app/f/main.do?show=7834639&infoWord=Yes+Go&refN=98876&code=rk1lhTLn266YBx9$$\n/app/sp2/mo.re?name=+\$m?ore&n=b\n/app/test/__JDbhLdSs4qpOhE1LmbspOhELmbhuV.Ss__/b;XXX=Abc!-22!-33!44!55" | ./qsfilter $@
 
