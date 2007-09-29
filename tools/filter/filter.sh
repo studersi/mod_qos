b9107530dc77637458c78773e9c608323df90758
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-09-29 20:09:39 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@165 49bada20-169a-462c-a56a-03074c89f563

--- tools/filter/filter.sh
+++ tools/filter/filter.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 
-echo -e "/app/test\n/other\n/m/this?name=val\n/app/mo^\"'re?name=value\n/app/sp;v1.1%20?show_all\n/app/f/main.do?show=7834639&infoWord=Yes+Go&refN=98876&code=rk1lhTLn266YBx9$$\n/app/sp2/mo.re?name=+\$m?ore&n=b\n/app/test/__JDbhLdSs4qpOhE1LmbspOhELmbhuV.Ss__/b;XXX=Abc!-22!-33!44!55" | ./qsfilter $@
+echo "/app/test" > access_log
+echo "/other" >> access_log
+echo "/m/this?name=val" >> access_log
+echo "/app/mo^\"'re?name=value" >> access_log
+echo "/app/sp;v1.1%20?show_all" >> access_log
+echo "/app/f/main.do?show=7834639&infoWord=Yes+Go&refN=98876&code=rk1lhTLn266YBx9$$" >> access_log
+echo "/app/sp2/mo.re?name=+\$m?ore&n=b" >> access_log
+echo "/app/test/__JDbhLdSs4qpOhE1LmbspOhELmbhuV.Ss__/b;XXX=Abc!-22!-33!44!55" >> access_log
+echo "/app/test/__JDbhLdSs4qpOhE1LmbspOhELmbhuV.Ss__/b;XXX=Abc!-22!-33!44!55888888" >> access_log
+
+./qsfilter -i access_log $@
 
