10bdc4b2cbc21389bba2602bd36b30007afddf20
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2007-10-18 09:15:46 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@195 49bada20-169a-462c-a56a-03074c89f563

--- tools/filter/filter2.sh
+++ tools/filter/filter2.sh
@@ -6,6 +6,7 @@ echo "/m/this?name=val&more" >> access_log
 echo "/app/mo^\"'re?name=value" >> access_log
 echo "/app/this/cont" >> access_log
 echo "/app/this/cont?name%c4=value%c3" >> access_log
+echo "/app/this/cont2?name%c4=value%c3" >> access_log
 echo "/app/sp;v1.1%20?show_all" >> access_log
 echo "/app/f/main.do?show=7834639&infoWord=Yes+Go&refN=98876&code=rk1lhTLn266YBx9$$" >> access_log
 echo "/app/sp2ä/mo.reö?name=+\$m?ore&n=b" >> access_log
@@ -13,7 +14,7 @@ echo "/app/test/__JDbhLdSs4dpOhE1LmbspyhEshuV.Ss__/b;XXX=Abc!-22!-33!44!55" >> a
 echo "/app/test/__JDbhLdSs4qpOhE1LmbspOhELmbhuV.Ss__/b;XXX=Abc!-22!-33!44!55888888%C3" >> access_log
 echo "/app/test/__JDbhLdSs4qpOhE1kajji78oJa8HLmbsp_-*other?name=value" >> access_log
 echo "/app/test/sub?n=v#u?as" >> access_log
-echo "//app/test/sub?n=http://a.b.c/i.t??" >> access_log
+echo "//app/test/?n=http://a.b.c/i.t??" >> access_log
 echo "/?n=n" >> access_log
 
 ./qsfilter2 -i access_log $@
