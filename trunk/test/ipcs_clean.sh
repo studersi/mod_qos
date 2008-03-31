bbc52fea35b5092b5660738aa2480599e231db77
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2008-03-31 20:23:48 +0000

    *** empty log message ***

--- trunk/test/ipcs_clean.sh
+++ trunk/test/ipcs_clean.sh
@@ -23,11 +23,13 @@ QS_UID_STR=`expr "$QS_UID" : 'uid=[0-9]*.\([a-z,A-Z,0-9,_]*\)'`
 
 echo "$PFX: delete shared memory segments"
 for E in `ipcs -m | grep $QS_UID_STR | awk '{print $2}'`; do
+    ipcs -m | grep $QS_UID_STR | head -1
     ipcrm -m $E
 done
 
 echo "$PFX: delete semaphore arrays"
 for E in `ipcs -s | grep $QS_UID_STR | awk '{print $2}'`; do
+    ipcs -s | grep $QS_UID_STR | head -1
     ipcrm -s $E
 done
 
