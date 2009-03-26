b4805b9ee51d7cdf274dd19913f72d407aa094de
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2009-03-26 09:52:43 +0000

    *** empty log message ***

--- trunk/tools/filter/qsfilter2.c
+++ trunk/tools/filter/qsfilter2.c
@@ -24,8 +24,8 @@
  *
  */
 
-static const char revision[] = "$Id: qsfilter2.c,v 1.107 2009-03-13 20:45:18 pbuchbinder Exp $";
-static const char g_revision[] = "8.10";
+static const char revision[] = "$Id: qsfilter2.c,v 1.108 2009-03-26 09:52:43 pbuchbinder Exp $";
+static const char g_revision[] = "8.11";
 
 /* system */
 #include <stdio.h>
