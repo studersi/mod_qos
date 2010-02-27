05f0d8262180b8037bbfc4d40d5aa846220eb23e
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2010-02-27 08:58:19 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_9_9'.

---
+++ tags/REL_9_9/test/simple.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# -*-mode: ksh; ksh-indent: 2; -*-
+
+./ctl.sh stop
+sleep 1
+set -e
+set -u
+echo "QOS Apache"
+ulimit -c unlimited
+../httpd/httpd -d `pwd` -f conf/simple.conf
+#../httpd/httpd -d `pwd` -f appl_conf/httpd.conf
+sleep 1
+
+time ./run.sh -s scripts/simple.htt
+
+./ctl.sh stop
+sleep 1
+echo "Apache"
+../httpd/httpd -d `pwd` -f conf/simple.conf -D no_qos
+#../httpd/httpd -d `pwd` -f appl_conf/httpd.conf
+sleep 1
+
+time ./run.sh -s scripts/simple.htt
+
+./ctl.sh stop
+echo "normal end"
