be93547ce7100a38a9642169532529b68019f86c
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-03-01 19:31:23 +0000

    snapshot

--- trunk/test/simple.sh
+++ trunk/test/simple.sh
@@ -8,16 +8,19 @@ set -u
 echo "QOS Apache"
 ulimit -c unlimited
 ../httpd/httpd -d `pwd` -f conf/simple.conf
-#../httpd/httpd -d `pwd` -f appl_conf/httpd.conf
 sleep 1
 
+for E in `seq 100`; do
+  ./run.sh -s scripts/simple.htt
+done
 time ./run.sh -s scripts/simple.htt
+sleep 1
+./run.sh -s scripts/simple_verify.htt
 
 ./ctl.sh stop
 sleep 1
 echo "Apache"
 ../httpd/httpd -d `pwd` -f conf/simple.conf -D no_qos
-#../httpd/httpd -d `pwd` -f appl_conf/httpd.conf
 sleep 1
 
 time ./run.sh -s scripts/simple.htt
