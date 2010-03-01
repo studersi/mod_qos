5ad3481e94942feff8af4f19e92a03a5e641cd88
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-03-01 20:34:35 +0000

    *** empty log message ***

--- trunk/test/test.sh
+++ trunk/test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Header: /home/cvs/m/mo/mod-qos/src/test/test.sh,v 2.102 2010-02-27 09:28:17 pbuchbinder Exp $
+# $Header: /home/cvs/m/mo/mod-qos/src/test/test.sh,v 2.103 2010-03-01 20:34:35 pbuchbinder Exp $
 #
 # mod_qos test cases, requires htt, see http://htt.sourceforge.net/
 #
@@ -329,56 +329,10 @@ if [ $? -ne 0 ]; then
     ERRORS=`expr $ERRORS + 1`
     echo "FAILED QS_HeaderFilter.htt"
 fi
-
 # -----------------------------------------------------------------
-./ctl.sh restart -D max_clients -D cc > /dev/null
-echo "-- QS_ClientPrefer.htt" >>  logs/error_log
-./run.sh scripts/Log.htt > /dev/null
-QSTART=`grep -c "mod_qos(063)" logs/error_log`
-echo "run ./scripts/QS_ClientPrefer.htt"
-./run.sh -s ./scripts/QS_ClientPrefer.htt 2>/dev/null 1>/dev/null
-sleep 1
-./run.sh scripts/Log.htt > /dev/null
-sleep 1
-QFIRST=`grep -c "mod_qos(063)" logs/error_log`
-./run.sh -s ./scripts/QS_ClientPrefer2.htt 2>/dev/null 1>/dev/null
-sleep 1
-./run.sh scripts/Log.htt > /dev/null
-sleep 1
-QSECOND=`grep -c "mod_qos(063)" logs/error_log`
-./run.sh -s ./scripts/QS_ClientPrefer_IP.htt 2>/dev/null 1>/dev/null
-sleep 1
-./run.sh scripts/Log.htt > /dev/null
-sleep 1
-QSTHIRD=`grep -c "mod_qos(063)" logs/error_log`
-QDIFF1=`expr $QFIRST - $QSTART`
-QDIFF2=`expr $QSECOND - $QFIRST`
-QDIFF3=`expr $QSTHIRD - $QSECOND`
-echo "$QDIFF1 $QDIFF2 $QDIFF3"
-if [ $QDIFF1 -lt $QDIFF2 ]; then
-    ERRORS=`expr $ERRORS + 1`
-    echo "FAILED QS_ClientPrefer.htt"
-fi
-if [ $QDIFF2 -lt $QDIFF3 ]; then
-    ERRORS=`expr $ERRORS + 1`
-    echo "FAILED QS_ClientPrefer_IP.htt"
-fi
-QSTART=`grep -c "mod_qos(064)" logs/error_log`
-./htt.sh -se ./scripts/QS_ClientPrefer_SP.htt
-QFIRST=`grep -c "mod_qos(064)" logs/error_log`
-QDIFF1=`expr $QFIRST - $QSTART`
-echo "$QDIFF1"
-if [ $QDIFF1 -eq 0 ]; then
-    ERRORS=`expr $ERRORS + 1`
-    echo "FAILED QS_ClientPrefer_SP.htt"
-fi
-#./ctl.sh restart -D real_ip -D cc > /dev/null
-./ctl.sh restart -D cc > /dev/null
-./htt.sh -se ./scripts/ClientBehavior.htt
-if [ $? -ne 0 ]; then
-    ERRORS=`expr $ERRORS + 1`
-    echo "FAILED ClientBehavior.htt"
-fi
+./prefer.sh
+EXT_ERR=$?
+ERRORS=`expr $ERRORS + $EXT_ERR`
 
 echo "-- QS_SetEnvResHeaders" >> logs/error_log
 ./htt.sh -se ./scripts/QS_SetEnvResHeaders.htt
