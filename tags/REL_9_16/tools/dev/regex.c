71260354176326e4e846171ce6f820defdc817ab
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2010-04-22 18:17:37 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_9_16'.

---
+++ tags/REL_9_16/tools/dev/regex.c
@@ -0,0 +1,91 @@
+/**
+ * See http://sourceforge.net/projects/mod-qos/ for further
+ * details.
+ *
+ * Copyright (C) 2007-2009 Pascal Buchbinder
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
+static const char revision[] = "$Id: regex.c,v 1.3 2010-03-03 19:18:40 pbuchbinder Exp $";
+
+/* system */
+#include <stdio.h>
+#include <string.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+/* OpenSSL  */
+#include <openssl/stack.h>
+
+/* apr */
+#include <pcre.h>
+#include <apr.h>
+#include <apr_uri.h>
+#include <apr_signal.h>
+#include <apr_strings.h>
+#include <apr_network_io.h>
+#include <apr_file_io.h>
+#include <apr_time.h>
+#include <apr_getopt.h>
+#include <apr_general.h>
+#include <apr_lib.h>
+#include <apr_portable.h>
+#include <apr_thread_proc.h>
+#include <apr_thread_cond.h>
+#include <apr_thread_mutex.h>
+#include <apr_support.h>
+
+#define MAX_LINE 32768
+#define CR 13
+#define LF 10
+
+static void usage() {
+  printf("usage: regex <string> <pcre>\n");
+  printf("\n");
+  printf("Regex matching test.\n");
+  printf("\n");
+  printf("See http://mod-qos.sourceforge.net/ for further details.\n");
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  const char *errptr = NULL;
+  int erroffset;
+  pcre *pcre;
+  int rc_c;
+
+  argc--;
+  argv++;
+  if(argc != 2) {
+    usage();
+  }
+
+  pcre = pcre_compile(argv[1], PCRE_DOTALL|PCRE_CASELESS, &errptr, &erroffset, NULL);
+  if(pcre == NULL) {
+    fprintf(stderr, "ERROR, rule <%s> could not compile pcre at position %d,"
+	    " reason: %s\n", argv[1], erroffset, errptr);
+    exit(1);
+  }
+  
+  rc_c = pcre_exec(pcre, NULL, argv[0], strlen(argv[0]), 0, 0, NULL, 0);
+
+  printf("%d\n", rc_c);
+  return rc_c;
+}
