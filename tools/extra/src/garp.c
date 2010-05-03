1d1bc2a74df270300ddcbf929828d419c329ef1c
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-05-03 19:27:58 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@820 49bada20-169a-462c-a56a-03074c89f563

--- tools/extra/src/garp.c
+++ tools/extra/src/garp.c
@@ -1,6 +1,7 @@
 /*
- * gratuitous arp:
- * ./send_arp eth1 192.168.1.166 ff:ff:ff:ff:ff:ff
+ * $Header: /home/cvs/m/mo/mod-qos/src/tools/extra/src/garp.c,v 1.1 2010-05-03 19:27:08 pbuchbinder Exp $
+ *
+ * send gratuitous arp
  */
 
 #include <arpa/inet.h>
@@ -24,10 +25,11 @@ void atoip(const char *s, struct in_addr *i) {
   struct hostent *h;
   i->s_addr = inet_addr(s);
   if (i->s_addr == -1) {
-    if ((h = gethostbyname(s)))
+    if ((h = gethostbyname(s))) {
       memcpy(i, h->h_addr, sizeof(i));
-    else
+    } else {
       error(1, 0, "unknown host '%s'", s);
+    }
   }
 }
 
@@ -58,22 +60,19 @@ void atohw(const char *s, u_int8_t *h) {
 }
 
 void usage(char *progname) {
-  fprintf(stderr, "\
-Usage: %s [OPTIONS] INTERFACE SENDER-IP SENDER-HW [TARGET-IP TARGET-HW]\n\
-Options:\n\
-  -i N  send arp broadcast every N milliseconds\n\
-\n\
-TARGET-IP and TARGET HW default to SENDER-IP and ff:ff:ff:ff:ff:ff for\n\
-gratuitous arp broadcast.\n\
-", progname);
+  fprintf(stderr, "Usage: %s <interface> <src ip> <src mac>\n", progname);
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Example:\n");
+  fprintf(stderr, " %s eth1 192.168.23.12 0b:ef:e6:47:ae:a7\n", progname);
+  fprintf(stderr, "\n");
   exit(1);
 }
 
 int main(int argc, char **argv) {
   int i = 0, opt, s;
-  char *progname = argv[0];
   struct ifreq ifr;
   struct sockaddr_ll sa;
+  char *progname = argv[0];
   struct {
     struct ether_header eh;
     struct arphdr ah;
@@ -82,63 +81,44 @@ int main(int argc, char **argv) {
     u_int8_t ar_tha[ETH_ALEN];
     struct in_addr ar_tip;
     u_int8_t padding[18];
-  } p;
+  } arp;
 
-  while ((opt = getopt(argc, argv, "i:")) > 0)
-    switch (opt) {
-      case 'i':
-        i = atoi(optarg);
-        if (i == 0)
-          error(1, 0, "-i takes a positive integer argument");
-        i *= 1000;
-        break;
-      default:
-        usage(progname);
-    }
-  argc -= optind;
-  argv += optind;
-
-  if (argc != 3 && argc != 5)
+  if(argc < 4) {
     usage(progname);
-
-  atoip(argv[1], &p.ar_sip);
-  atohw(argv[2], p.eh.ether_shost);
-  atohw(argv[2], p.ar_sha);
-  if (argc > 4) {
-    atoip(argv[3], &p.ar_tip);
-    atohw(argv[3], p.eh.ether_dhost);
-    atohw(argv[4], p.ar_tha);
-  } else {
-    atoip(argv[1], &p.ar_tip);
-    atohw("ff:ff:ff:ff:ff:ff", p.eh.ether_dhost);
-    atohw("ff:ff:ff:ff:ff:ff", p.ar_tha);
   }
 
-  p.eh.ether_type = htons(ETHERTYPE_ARP);
-  p.ah.ar_hrd = htons(ARPHRD_ETHER);
-  p.ah.ar_pro = htons(ETHERTYPE_IP);
-  p.ah.ar_hln = ETH_ALEN;
-  p.ah.ar_pln = sizeof(struct in_addr);
-  p.ah.ar_op = htons(ARPOP_REPLY);
-  memset(&p.padding, 0, sizeof(p.padding));
+  atoip(argv[2], &arp.ar_sip);
+  atohw(argv[3], arp.eh.ether_shost);
+  atohw(argv[3], arp.ar_sha);
+
+  atoip(argv[2], &arp.ar_tip);
+  atohw("ff:ff:ff:ff:ff:ff", arp.eh.ether_dhost);
+  atohw("ff:ff:ff:ff:ff:ff", arp.ar_tha);
+
+  arp.eh.ether_type = htons(ETHERTYPE_ARP);
+  arp.ah.ar_hrd = htons(ARPHRD_ETHER);
+  arp.ah.ar_pro = htons(ETHERTYPE_IP);
+  arp.ah.ar_hln = ETH_ALEN;
+  arp.ah.ar_pln = sizeof(struct in_addr);
+  arp.ah.ar_op = htons(ARPOP_REPLY);
+  memset(&arp.padding, 0, sizeof(arp.padding));
 
   s = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_RARP));
   if (s < 0)
     error(1, errno, "socket");
 
-  strncpy (ifr.ifr_name, argv[0], IFNAMSIZ);
-  if (ioctl(s, SIOCGIFINDEX, &ifr) < 0)
+  strncpy (ifr.ifr_name, argv[1], IFNAMSIZ);
+  if(ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
     error(1, errno, "ioctl SIOCGIFINDEX");
+  }
 
   sa.sll_family = AF_PACKET;
   sa.sll_ifindex = ifr.ifr_ifindex;
   sa.sll_halen = ETH_ALEN;
 
-  while (1) {
-    if (sendto(s, &p, sizeof(p), 0, (struct sockaddr *) &sa, sizeof(sa)) < 0)
-      error(1, errno, "sendto");
-    if (i == 0)
-      exit(0);
-    usleep(i);
+  if(sendto(s, &arp, sizeof(arp), 0, (struct sockaddr *) &sa, sizeof(sa)) < 0) {
+    error(1, errno, "sendto");
   }
+
+  return 0;
 }
