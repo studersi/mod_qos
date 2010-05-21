38ad28ebfbbb26f018d3056f27970dd04710e22a
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-05-21 20:50:59 +0000

    snapshot
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@838 49bada20-169a-462c-a56a-03074c89f563

--- mgr/src/test/java/ch/joebar/qos/mgr/test/HaTest.java
+++ mgr/src/test/java/ch/joebar/qos/mgr/test/HaTest.java
@@ -4,6 +4,7 @@ import org.apache.log4j.BasicConfigurator;
 
 import junit.framework.TestCase;
 import ch.joebar.qos.mgr.net.ha.Controller;
+import ch.joebar.qos.mgr.net.ha.Transition;
 
 public class HaTest extends TestCase {
 
@@ -17,14 +18,20 @@ public class HaTest extends TestCase {
 				"127.0.0.1", "127.0.0.2");
 		Controller c2 = new Controller("lo", "255.0.0.0", addresses,
 				"127.0.0.2", "127.0.0.1");
+
 		Thread t1 = new Thread(c1);
 		t1.start();
-		for(int i = 0; i < 10; i++) {
-			Thread.sleep(1000);
-		}
-		
-		System.out.println("end");
-		c1.end();
+		Thread t2 = new Thread(c2);
+		t2.start();
+		Thread.sleep(20000);
+
+		c1.setTransition(Transition.TRANSFER);
+		Thread.sleep(20000);
+
 		c2.end();
+		Thread.sleep(20000);
+				
+		c1.end();
+		System.out.println("end");
 	}
 }
