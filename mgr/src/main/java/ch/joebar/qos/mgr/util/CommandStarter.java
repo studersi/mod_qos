a1f0bd5fbe0d2a90561c2f358229373d2342e213
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-05-22 12:12:38 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@841 49bada20-169a-462c-a56a-03074c89f563

--- mgr/src/main/java/ch/joebar/qos/mgr/util/CommandStarter.java
+++ mgr/src/main/java/ch/joebar/qos/mgr/util/CommandStarter.java
@@ -1,5 +1,7 @@
 package ch.joebar.qos.mgr.util;
 
+import java.io.IOException;
+
 public class CommandStarter {
 
 	
@@ -7,6 +9,12 @@ public class CommandStarter {
 	}
 	
 	public String callCommandToString(String[] cmdArray) {
+		try {
+			Runtime.getRuntime().exec(cmdArray);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "";
 	}
 }
--- mgr/src/test/bin/ha.sh
+++ mgr/src/test/bin/ha.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # -*-mode: ksh; ksh-indent: 2; -*-
 
-echo $@
+echo "$@" >> ha.log
 
