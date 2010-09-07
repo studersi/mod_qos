2741c189633f5b2a7d7348937ec68aabae19e33a
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-09-07 20:15:35 +0000

    *** empty log message ***

--- trunk/test/test.sh
+++ trunk/test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # -*-mode: ksh; ksh-indent: 2; -*-
 #
-# $Header: /home/cvs/m/mo/mod-qos/src/test/test.sh,v 2.127 2010-09-06 19:42:23 pbuchbinder Exp $
+# $Header: /home/cvs/m/mo/mod-qos/src/test/test.sh,v 2.128 2010-09-07 20:15:35 pbuchbinder Exp $
 #
 # mod_qos test cases, requires htt, see http://htt.sourceforge.net/
 #
@@ -572,6 +572,11 @@ fi
 if [ $? -ne 0 ]; then
     ERRORS=`expr $ERRORS + 1`
     echo "FAILED QS_UserTrackingCookieName.htt"
+fi
+./run.sh -s ./scripts/QS_UserTrackingCookieName2.htt
+if [ $? -ne 0 ]; then
+    ERRORS=`expr $ERRORS + 1`
+    echo "FAILED QS_UserTrackingCookieName2.htt"
 fi
 
 # MaxRequestsPerChild&QS_SrvMinDataRate
