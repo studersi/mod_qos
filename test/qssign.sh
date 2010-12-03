3bd7bd0890737cb1c7946f7c2ca9b7d4f69525f8
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2010-12-03 07:06:39 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@973 49bada20-169a-462c-a56a-03074c89f563

--- test/qssign.sh
+++ test/qssign.sh
@@ -8,36 +8,36 @@ cat logs/access_log | ../util/src/qssign -s 1234567890 > logs/signed_access_log
 echo "- good"
 cat logs/signed_access_log | ../util/src/qssign -s 1234567890 -v
 if [ $? -ne 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (1)"
   exit 1
 fi
 
 echo "- good (from prg)"
 cat logs/signed_access_log | ../util/src/qssign -S "echo 1234567890" -v
 if [ $? -ne 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (2)"
   exit 1
 fi
 
 echo "- wrong passphrase (from prg)"
 OUT=`cat logs/signed_access_log | ../util/src/qssign -S "echo 123467890" -v 2>&1`
 if [ $? -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (3) ($OUT)"
   exit 1
 fi
 if [ `echo $OUT | grep -c "invalid signature"` -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (4)"
   exit 1
 fi
 
 echo "- wrong passphrase"
 OUT=`cat logs/signed_access_log | ../util/src/qssign -s abc -v 2>&1`
 if [ $? -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (5)"
   exit 1
 fi
 if [ `echo $OUT | grep -c "invalid signature"` -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (6)"
   exit 1
 fi
 
@@ -45,11 +45,11 @@ echo "- invalid signature"
 sed <logs/signed_access_log >logs/signed_access_log.1 -e "s:000000000005#:000000000003#:g"
 OUT=`cat logs/signed_access_log.1 | ../util/src/qssign -s 1234567890 -v 2>&1`
 if [ $? -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (7)"
   exit 1
 fi
 if [ `echo $OUT | grep -c "invalid signature"` -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (8)"
   exit 1
 fi
 
@@ -57,11 +57,11 @@ echo "- invalid sequence (missing line)"
 grep -v "000000000003#" logs/signed_access_log > logs/signed_access_log.1
 OUT=`cat logs/signed_access_log.1 | ../util/src/qssign -s 1234567890 -v 2>&1`
 if [ $? -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (9)"
   exit 1
 fi
 if [ `echo $OUT | grep -c "wrong sequence"` -eq 0 ]; then
-  echo "ERROR: verification failed"
+  echo "ERROR: verification failed (10)"
   exit 1
 fi
 
