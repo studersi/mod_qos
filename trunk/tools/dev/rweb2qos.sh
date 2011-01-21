d0c7e388cd9eef426ed65f7e465b91ae6210fd2e
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2011-01-21 22:59:32 +0000

    inital version (slow)

--- trunk/tools/dev/rweb2qos.sh
+++ trunk/tools/dev/rweb2qos.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 # -*-mode: ksh; ksh-indent: 2; -*-
 #
-# $Id: rweb2qos.sh,v 1.4 2011-01-21 22:05:22 pbuchbinder Exp $
+# $Id: rweb2qos.sh,v 1.5 2011-01-21 22:59:32 pbuchbinder Exp $
 #
 
 declare -a A_NAME
 declare -a A_PATTERN
 
 IN=$1
+ONCE=1
 
 if [ -z "$IN" ]; then
   echo "Usage: `basename $0` <rule file>"
   exit 1
 fi
 
+printArray() {
+  if [ $ONCE -eq 1 ]; then
+    ONCE=0
+    pattern=$1
+    index=${#A_NAME[*]}
+    i=0
+    while [ $i -lt $index ]; do
+      n=${A_NAME[$i]}
+      p=${A_PATTERN[$i]}
+      echo "$i> $n $p"
+      let i=$i+1
+    done
+  fi
+}
+
+sortArray() {
+  index=${#A_NAME[*]}
+  i=0
+  let index=$index-1
+  while [ $i -lt $index ]; do
+    let j=$i+1
+    n1=${A_NAME[$i]}
+    p1=${A_PATTERN[$i]}
+    n2=${A_NAME[$j]}
+    p2=${A_PATTERN[$j]}
+    if [ `expr length $n1` -lt `expr length $n2` ]; then
+      A_NAME[$i]=$n2
+      A_NAME[$j]=$n1
+      A_PATTERN[$i]=$p2
+      A_PATTERN[$j]=$p1
+      i=0
+    fi
+    let i=$i+1
+  done
+}
+
 resolve() {
-  # TODO: sort (longest match first)
   pattern=$1
   index=${#A_NAME[*]}
   i=0
@@ -34,6 +70,7 @@ MAX=`wc -l $IN | awk '{print $1}'`
 count=0
 while [ $count -lt $MAX ]; do
   let count=$count+1
+  #echo "# line $count"
   line=`head -$count $IN | tail -1`
   name=`echo $line | awk '{print $1}'`
   if [ `echo $name | egrep -c "^#.*"` -gt 0 ]; then
@@ -43,6 +80,7 @@ while [ $count -lt $MAX ]; do
   if [ -n "$name" ]; then
     if [ $name = "GET" -o $name = "POST" ]; then
       # new rule:
+      #printArray
       path=`echo $line | cut -d ' ' -f 2`
       query=""
       all_query=`echo $line | cut -d ' ' -f 3-`
@@ -66,8 +104,10 @@ while [ $count -lt $MAX ]; do
       pattern=`echo $line | cut -d ' ' -f 2-`
       resolve $pattern
       #echo "$index add pattern [$name] [$RESOLVED]"
+      # TODO: insert instead of sort
       A_NAME[$index]="$name"
       A_PATTERN[$index]="$RESOLVED"
+      sortArray
     fi
   fi
 done
