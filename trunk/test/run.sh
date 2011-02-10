4c776ab31bfd07cc2b3b8413600127a25bce202d
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2011-02-10 20:12:35 +0000

    *** empty log message ***

--- trunk/test/run.sh
+++ trunk/test/run.sh
@@ -4,7 +4,7 @@ RC=0
 if [ "$1" = "-s" -o "$1" = "-se" ]; then
     LOG=`basename $2`
     echo -e "run $2\t\c"
-    ./bin/httest1 $2 > .${LOG}.log
+    ./bin/httest1 $2 2>&1 > .${LOG}.log
     RC=$?
     if [ $RC -ne 0 ]; then
 	echo "FAILED"
