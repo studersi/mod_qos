d222a7155742db18564d015a98b23f6e2df25de1
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2011-08-26 16:58:47 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1186 49bada20-169a-462c-a56a-03074c89f563

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # -*-mode: ksh; ksh-indent: 2; -*-
 #
-# $Header: /home/cvs/m/mo/mod-qos/src/test/test.sh,v 2.167 2011-07-22 18:55:52 pbuchbinder Exp $
+# $Header: /home/cvs/m/mo/mod-qos/src/test/test.sh,v 2.168 2011-08-26 16:58:47 pbuchbinder Exp $
 #
 # mod_qos test cases, requires htt, see http://htt.sourceforge.net/
 #
@@ -782,6 +782,16 @@ fi
 if [ $? -ne 0 ]; then
     ERRORS=`expr $ERRORS + 1`
     echo "FAILED qslog.htt"
+fi
+./qslog.sh test log4j
+if [ $? -ne 0 ]; then
+    ERRORS=`expr $ERRORS + 1`
+    echo "FAILED qslog.sh test log4j"
+fi
+./qslog.sh test apache
+if [ $? -ne 0 ]; then
+    ERRORS=`expr $ERRORS + 1`
+    echo "FAILED qslog.sh test apache"
 fi
 
 # end -------------------------------------------------------------
