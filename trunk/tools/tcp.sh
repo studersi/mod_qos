d18ad9d83ffc658f18f57ab4d43b05962d8f1467
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2012-01-24 07:18:34 +0000

    enhanced tcp/iptables settings

--- trunk/tools/tcp.sh
+++ trunk/tools/tcp.sh
@@ -5,6 +5,10 @@ if [ `uname -s` = "Linux" ]; then
   echo "32000"      > /proc/sys/net/core/netdev_max_backlog
   echo "4096 61000" > /proc/sys/net/ipv4/ip_local_port_range
   echo "30"         > /proc/sys/net/ipv4/tcp_fin_timeout
+  echo "1"          > /proc/sys/net/ipv4/tcp_window_scaling
+  #echo "0"          > /proc/sys/net/ipv4/tcp_sack
+  #echo "1280"       > /proc/sys/net/ipv4/tcp_max_syn_backlog
+  #echo "1800"       > /proc/sys/net/ipv4/tcp_keepalive_time
 fi
 
 if [ `uname -s` = "SunOS" ]; then
