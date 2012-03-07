0671407e9d0ccf357335b0e9837f3361cdc68503
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2012-03-07 20:24:49 +0000

    cont 24, MaxRequestWorkers
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1363 49bada20-169a-462c-a56a-03074c89f563

--- httpd_src/modules/qos/mod_qos.c
+++ httpd_src/modules/qos/mod_qos.c
@@ -40,7 +40,7 @@
 /************************************************************************
  * Version
  ***********************************************************************/
-static const char revision[] = "$Id: mod_qos.c,v 5.398 2012-03-07 19:23:13 pbuchbinder Exp $";
+static const char revision[] = "$Id: mod_qos.c,v 5.399 2012-03-07 20:24:49 pbuchbinder Exp $";
 static const char g_revision[] = "10.4";
 
 /************************************************************************
@@ -209,11 +209,13 @@ static char qs_magic[QOS_MAGIC_LEN] = "qsmagic";
 // Apache 2.4 compat
 #if (AP_SERVER_MINORVERSION_NUMBER == 4)
 // TODO
+#define QS_MaxClients "MaxRequestWorkers"
 #define WORKER_MPM 1
 #define QS_CONN_REMOTEIP(c) c->client_ip
 #define QOS_MY_GENERATION(g) ap_mpm_query(AP_MPMQ_GENERATION, &g)
 #define qos_unixd_set_global_mutex_perms ap_unixd_set_global_mutex_perms
 #else
+#define QS_MaxClients "MaxClients"
 #define QS_CONN_REMOTEIP(c) c->remote_ip
 #define QOS_MY_GENERATION(g) g = ap_my_generation
 #define qos_unixd_set_global_mutex_perms unixd_set_global_mutex_perms
@@ -7793,7 +7795,7 @@ static int qos_post_config(apr_pool_t *pconf, apr_pool_t *plog, apr_pool_t *ptem
   qos_hostcode(ptemp, bs);
   QOS_MY_GENERATION(sconf->act->generation);
   for (pdir = ap_conftree; pdir != NULL; pdir = pdir->next) {
-    if(strcasecmp(pdir->directive, "MaxClients") == 0) {
+    if(strcasecmp(pdir->directive, QS_MaxClients) == 0) {
       net_prefer = atoi(pdir->args);
       sconf->max_clients = net_prefer;
     }
@@ -7808,7 +7810,7 @@ static int qos_post_config(apr_pool_t *pconf, apr_pool_t *plog, apr_pool_t *ptem
   }
   if(net_prefer <= 1) {
     ap_log_error(APLOG_MARK, APLOG_EMERG, 0, bs, 
-                 QOS_LOG_PFX(007)"could not determine MaxClients!"
+                 QOS_LOG_PFX(007)"could not determine "QS_MaxClients"!"
                  " You MUST set this directive within the Apache configuration file.");
   }
 #ifdef WORKER_MPM
