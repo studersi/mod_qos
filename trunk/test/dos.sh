521adb910c3441969e6a957716fb810c8bede57b
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2012-09-24 19:06:34 +0000

    *** empty log message ***

--- trunk/test/dos.sh
+++ trunk/test/dos.sh
@@ -21,3 +21,32 @@ sleep 2
 ./run.sh -s scripts/dos_keepalive.htt
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+# performance with loaded mod_qos (mod_proxy)
+../httpd/httpd -d `pwd` -f conf/dos.conf 2>/dev/null 1>/dev/null
+sleep 2
+t1=`date '+%s'`
+./run.sh -s scripts/dos_perf.htt
+t2=`date '+%s'`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+# performance WITHOUT mod_qos
+../httpd/httpd -d `pwd` -f conf/dos.conf -D no_qos 2>/dev/null 1>/dev/null
+sleep 2
+t3=`date '+%s'`
+./run.sh -s scripts/dos_perf.htt
+t4=`date '+%s'`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+tw=`expr $t2 - $t1`
+to=`expr $t4 - $t3`
+echo " with: $tw, without: $to"
+dif=`expr $tw - $to`
+set +e
+# up to 1% slower (incl rounding) is still okay (since the server
+# has not really anything else to do)
+if [ $dif -gt 2 ]; then
+  echo " dos.sh test was to slow"
+  exit 1
+fi
+
+exit 0
