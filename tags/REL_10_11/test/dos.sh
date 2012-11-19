02efe35c8701a85987a5cc549e9e9fc849c3cf17
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2012-11-19 21:25:25 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_10_11'.

---
+++ tags/REL_10_11/test/dos.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# -*-mode: ksh; ksh-indent: 2; -*-
+
+./ctl.sh stop 2>/dev/null 1>/dev/null
+set -e
+set -u
+ulimit -c unlimited
+
+../httpd/httpd -d `pwd` -f conf/dos.conf 2>/dev/null 1>/dev/null
+sleep 2
+./run.sh -s scripts/QS_SrvRequestRate_0.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/dos.conf 2>/dev/null 1>/dev/null
+sleep 2
+./run.sh -s scripts/dos_session.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/dos.conf 2>/dev/null 1>/dev/null
+sleep 2
+./run.sh -s scripts/dos_keepalive.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+# performance with loaded mod_qos (mod_proxy)
+../httpd/httpd -d `pwd` -f conf/dos.conf 2>/dev/null 1>/dev/null
+sleep 2
+t1=`date '+%s'`
+./run.sh -s scripts/dos_perf.htt
+t2=`date '+%s'`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+# performance WITHOUT mod_qos
+../httpd/httpd -d `pwd` -f conf/dos.conf -D no_qos 2>/dev/null 1>/dev/null
+sleep 2
+t3=`date '+%s'`
+./run.sh -s scripts/dos_perf.htt
+t4=`date '+%s'`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+tw=`expr $t2 - $t1`
+to=`expr $t4 - $t3`
+echo " with: $tw, without: $to"
+dif=`expr $tw - $to`
+set +e
+# up to 1% slower (incl rounding) is still okay (since the server
+# has not really anything else to do)
+if [ $dif -gt 2 ]; then
+  echo " dos.sh test was too slow"
+  exit 1
+fi
+
+exit 0
