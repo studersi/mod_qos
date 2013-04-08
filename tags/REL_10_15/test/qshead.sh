e9cb43f0b1e046779c3c68c522a5193c8ef67a2f
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2013-04-08 18:44:31 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_10_15'.

---
+++ tags/REL_10_15/test/qshead.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# -*-mode: ksh; ksh-indent: 2; -*-
+
+cd `dirname $0`
+PFX=[`basename $0`]
+
+echo "$PFX"
+
+lines=`printf "123\n456\n789\n" | ../util/src/qshead -p 456 | wc -l`
+if [ $lines -ne 2 ]; then
+  echo "$PFX FAILED $lines"
+  exit 1
+fi
+
+exit 0
