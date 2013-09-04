14c2fb4d564fcb96067aee8def58a7fd6f9dbbc8
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2013-09-04 16:21:54 +0000

    uc1a/b

--- trunk/test/uc1.sh
+++ trunk/test/uc1.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # -*-mode: ksh; ksh-indent: 2; -*-
+#
+# execute tests using config for single use cases (only one feature enabled)
+#
 
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 set -e
 set -u
 ulimit -c unlimited
 
-../httpd/httpd -d `pwd` -f conf/uc1.conf 2>/dev/null 1>/dev/null
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1a 2>/dev/null 1>/dev/null
 sleep 2
 ./run.sh -s scripts/UC1_QS_LocRequestLimitMatch.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1b 2>/dev/null 1>/dev/null
+sleep 2
+./run.sh -s scripts/UC1_QS_ClientEventLimitCount.htt
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
 exit 0
