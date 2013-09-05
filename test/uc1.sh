7950adb298c853ab6c81047ba42eddd9ffe22db0
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2013-09-05 19:58:34 +0000

    fix QS_CondLocRequestLimitMatch
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1542 49bada20-169a-462c-a56a-03074c89f563

--- test/uc1.sh
+++ test/uc1.sh
@@ -19,4 +19,9 @@ sleep 2
 ./run.sh -s scripts/UC1_QS_ClientEventLimitCount.htt
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1c 2>/dev/null 1>/dev/null
+sleep 2
+./run.sh -s scripts/UC1_QS_CondLocRequestLimitMatch.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 exit 0
