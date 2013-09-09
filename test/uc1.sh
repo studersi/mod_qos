cf14b2267a77449a383c4038fdf1db408440ab9d
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2013-09-09 16:19:34 +0000

    UC1 ErrorPage
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1547 49bada20-169a-462c-a56a-03074c89f563

--- test/uc1.sh
+++ test/uc1.sh
@@ -56,4 +56,9 @@ waitApache
 ./run.sh -s scripts/UC1_QS_RedirectIf.htt
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1h 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_ErrorPage.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 exit 0
