abdbfcf756d8dc1abd029a2c962ce7729d23b431
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2013-10-29 07:37:40 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_10_24'.

---
+++ tags/REL_10_24/test/uc1.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+# -*-mode: ksh; ksh-indent: 2; -*-
+#
+# execute tests using config for single use cases (only one feature enabled)
+#
+
+./ctl.sh stop 2>/dev/null 1>/dev/null
+set -e
+set -u
+ulimit -c unlimited
+
+waitApache() {
+  COUNT=0
+  while [ $COUNT -lt 20 ]; do
+    if [ -f logs/apache.pid ]; then
+      COUNT=20
+    else
+      let COUNT=$COUNT+1
+      ./bin/sleep 200
+    fi
+  done
+}
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1a 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_LocRequestLimitMatch.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1b 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_ClientEventLimitCount.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1c 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_CondLocRequestLimitMatch.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1d 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_LocRequestLimit.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1e 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_ClientEventBlockCount.htt
+./run.sh -s scripts/UC1_QS_ClientEventBlockCount2.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1f 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_EventRequestLimit.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1g 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_RedirectIf.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1h 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_ErrorPage.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1i 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_LocRequestPerSecLimit.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1j 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_EventLimitCount.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1k 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_Milestone.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1l 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_LocKBytesPerSecLimitMatch.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1m 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_SrvMaxConn.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1n 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_SrvMaxConnClose.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1o 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_EventPerSecLimit.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1p 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_CondClientEventLimitCount.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1p -D uc1q 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_CondClientEventLimitCount2.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+exit 0
---
+++ tags/REL_10_24/test/ucn.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# -*-mode: ksh; ksh-indent: 2; -*-
+#
+# execute tests using config for use cases (using multiple features/parameters)
+#
+
+./ctl.sh stop 2>/dev/null 1>/dev/null
+set -e
+set -u
+ulimit -c unlimited
+
+waitApache() {
+  COUNT=0
+  while [ $COUNT -lt 20 ]; do
+    if [ -f logs/apache.pid ]; then
+      COUNT=20
+    else
+      let COUNT=$COUNT+1
+      ./bin/sleep 200
+    fi
+  done
+}
+
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucna 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_LocRequestLimit.htt
+./run.sh -s scripts/UCN_QS_LocRequestLimit2.htt
+./run.sh -s scripts/UCN_QS_LocRequestLimit3.htt
+./run.sh -s scripts/UCN_QS_LocKBytesPerSecLimit.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucnb 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_ClientEventLimitCount.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucnc 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_ClientEventLimitCount2.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucnd 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_ClientEventLimitCount3.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucnd -D ucne 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_ClientEventLimitCount4.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
+exit 0
