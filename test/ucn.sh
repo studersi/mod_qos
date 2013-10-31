28c4f7e3126a44f6144ff9eaea314466782472c2
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2013-10-31 13:06:23 +0000

    QS_IPConn
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1582 49bada20-169a-462c-a56a-03074c89f563

--- test/ucn.sh
+++ test/ucn.sh
@@ -56,5 +56,10 @@ waitApache
 ./run.sh -s scripts/UCN_QS_EventLimitCount.htt
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucng 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_IPConn.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 echo "$PFX normal end"
 exit 0
