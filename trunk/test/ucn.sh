09247d879152d12b366ddf8bb7a59814d0959a3b
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2013-11-20 06:56:06 +0000

    QS_ClientSerialize supports the QS_ClientIpFromHeader

--- trunk/test/ucn.sh
+++ trunk/test/ucn.sh
@@ -71,5 +71,10 @@ waitApache
 ./run.sh -s scripts/UCN_QS_ClientSerialize.htt
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D ucnj 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UCN_QS_ClientSerialize2.htt
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 echo "$PFX normal end"
 exit 0
