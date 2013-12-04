226d999f060874d94eda9aee3a681a47bd2aabbd
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2013-12-04 20:05:49 +0000

    pu/puc

--- trunk/util/src/qs_util.h
+++ trunk/util/src/qs_util.h
@@ -29,7 +29,7 @@
 /* ----------------------------------
  * version info
  * ---------------------------------- */
-static const char man_version[] = "10.26";
+static const char man_version[] = "10.27";
 static const char man_date[] = "December 2013";
 
 /* ----------------------------------
