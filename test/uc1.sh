54073dbacd802e5052fd4ae915ef2d0709618fc4
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2014-01-21 21:07:22 +0000

    uc1
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1635 49bada20-169a-462c-a56a-03074c89f563

--- test/uc1.sh
+++ test/uc1.sh
@@ -154,6 +154,12 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1w 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_ClientSerialize.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 if [ $ERRORS -ne 0 ]; then
   echo "$PFX test failed with $ERRORS errors"
   exit $ERRORS
