f74b3cd606110aee26078197e4df48fa25ee9239
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2014-03-25 11:57:09 +0000

    exec sample

--- trunk/test/sync.sh
+++ trunk/test/sync.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+#
+# Example to propagte QS_ClientEventLimitCount events (client
+# is blocked) to another (redundant/hot standby) server.
+#
+# Usage: 
+# ErrorLog  "|qsexec -e \'mod_qos.067.: access denied, QS_ClientEventLimitCount rule: event=(.*), max=[0-9]+, current=[0-9]+, c=(.*), id=\' -p \'sync.sh $1 $2\' |qsrotate -o logs/error_log"
+#
 
 cd `dirname $0`
 . ./ports
-wget -O - http://127.0.0.1:$QS_PORT_BASE6/console?action=limit\&address=$2\&event=$1 >/dev/null
-
+wget -O - http://127.0.0.1:$QS_PORT_BASE6/console?action=limit\&address=$2\&event=$1 >/dev/null &
