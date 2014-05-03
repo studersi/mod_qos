667ac464f01830266e261935ed45c21d942060a4
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2014-05-03 11:30:33 +0000

    *** empty log message ***

--- trunk/test/uc1.sh
+++ trunk/test/uc1.sh
@@ -97,6 +97,12 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1l2 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_LocKBytesPerSecLimitMatch2.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 ../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1m 2>/dev/null 1>/dev/null
 waitApache
 ./run.sh -s scripts/UC1_QS_SrvMaxConn.htt
