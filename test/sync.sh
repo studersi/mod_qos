9e37a509aafeac11d0db783910cfef32dbe947ae
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2014-06-17 20:08:43 +0000

    sync sample
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1732 49bada20-169a-462c-a56a-03074c89f563

--- test/sync.sh
+++ test/sync.sh
@@ -3,10 +3,22 @@
 # Example to propagte QS_ClientEventLimitCount events (client
 # is blocked) to another (redundant/hot standby) server.
 #
-# Usage: 
-# ErrorLog  "|qsexec -e \'mod_qos.067.: access denied, QS_ClientEventLimitCount rule: event=(.*), max=[0-9]+, current=[0-9]+, c=(.*), id=\' -p \'sync.sh $1 $2\' |qsrotate -o logs/error_log"
+# Sample usage/configuration:
+# QS_ClientEventLimitCount  10 60  QS_Limit
+# QS_ClientEventLimitCount  12 120 QS_LimitSP
+# ErrorLog  "|qsexec -e \'mod_qos.067.: access denied, QS_ClientEventLimitCount rule: event=(.*), max=[0-9]+, current=[0-9]+, c=(.*), id=\' -p \'sync.sh $1 $2 $3\' |qsrotate -o logs/error_log"
 #
 
 cd `dirname $0`
 . ./ports
-wget -O - http://127.0.0.1:$QS_PORT_BASE6/console?action=limit\&address=$2\&event=$1 >/dev/null &
+
+if [ $1 = "QS_Limit" -a $2 != "10" ]; then
+    # propagte status only once (when reaching the limit)
+    exit 0
+fi
+if [ $1 = "QS_LimitSP" -a $2 != "12" ]; then
+    # propagte status only once (when reaching the limit)
+    exit 0
+fi
+
+wget -O - http://127.0.0.1:$QS_PORT_BASE6/console?action=limit\&address=$3\&event=$1 >/dev/null &
