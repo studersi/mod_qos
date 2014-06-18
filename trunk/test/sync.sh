ebb260436c14586d66db58d48dc58ed76692d979
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2014-06-18 19:05:54 +0000

    060/067

--- trunk/test/sync.sh
+++ trunk/test/sync.sh
@@ -6,18 +6,14 @@
 # Sample usage/configuration:
 # QS_ClientEventLimitCount  10 60  QS_Limit
 # QS_ClientEventLimitCount  12 120 QS_LimitSP
-# ErrorLog  "|qsexec -e \'mod_qos.067.: access denied, QS_ClientEventLimitCount rule: event=(.*), max=[0-9]+, current=[0-9]+, c=(.*), id=\' -p \'sync.sh $1 $2 $3\' |qsrotate -o logs/error_log"
+# ErrorLog  "|qsexec -e \'mod_qos.067.: access denied, QS_ClientEventLimitCount rule: event=(.*), max=[0-9]+, current=[0-9]+, age=0, c=(.*), id=\' -p \'sync.sh $1 $2 $3\' |qsrotate -o logs/error_log"
 #
 
 cd `dirname $0`
 . ./ports
 
-if [ $1 = "QS_Limit" -a $2 != "10" ]; then
-    # propagte status only once (when reaching the limit)
-    exit 0
-fi
-if [ $1 = "QS_LimitSP" -a $2 != "12" ]; then
-    # propagte status only once (when reaching the limit)
+if [ $2 -gt 1000 ]; then
+    # don't propagte the status if set by console
     exit 0
 fi
 
