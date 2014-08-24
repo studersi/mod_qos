b2418fb1c1f9cdfc3dad5288119c2c77f526d4ca
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2014-08-24 20:09:13 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_11_5'.

---
+++ tags/REL_11_5/test24/test.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# -*-mode: ksh; ksh-indent: 2; -*-
+
+cd `dirname $0`
+
+if [ `ps -ef | grep -v grep | grep -c "tee test.log"` -eq 0 ]; then
+  $0 | tee test.log
+  exit $?
+fi
+
+ulimit -c unlimited
+./generate.sh
+. ./ports
+
+ERRORS=0
+WARNINGS=0
+
+# delete the access log file since it is used to generate permit rules
+./ctl.sh stop > /dev/null
+sleep 1
+IPCS=`ipcs | wc -l`
+rm -f logs/*
+
+echo "start (`date '+%a %b %d %H:%M:%S %Y'`)"
+./ctl.sh start > /dev/null
+
+for E in `ls scripts/*.htt | sort`; do
+  ./run.sh -s $E
+  if [ $? -ne 0 ]; then
+    ERRORS=`expr $ERRORS + 1`
+    echo "FAILED $E"
+  fi
+done
+
+./ctl.sh stop > /dev/null
+echo "end (`date '+%a %b %d %H:%M:%S %Y'`)"
+
+if [ $WARNINGS -ne 0 ]; then
+    echo "ERROR: got $WARNINGS warnings and $ERRORS errors"
+    exit 1
+fi
+
+if [ $ERRORS -ne 0 ]; then
+    echo "ERROR: end with $ERRORS errors"
+    exit 1
+fi
+
+echo "normal end"
+exit 0
+
