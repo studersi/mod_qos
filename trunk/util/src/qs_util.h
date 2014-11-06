1212cd0b03b44c01836bbffeda6908ec2e4c076c
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2014-11-06 18:46:48 +0000

    maint release

--- trunk/util/src/qs_util.h
+++ trunk/util/src/qs_util.h
@@ -29,8 +29,8 @@
 /* ----------------------------------
  * version info
  * ---------------------------------- */
-static const char man_version[] = "11.5";
-static const char man_date[] = "August 2014";
+static const char man_version[] = "11.6";
+static const char man_date[] = "November 2014";
 
 /* ----------------------------------
  * definitions
