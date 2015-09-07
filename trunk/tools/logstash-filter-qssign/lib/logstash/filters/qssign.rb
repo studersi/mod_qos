633fcfd11af3cc6a7020a3be100f47c8f177c9b5
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2015-09-07 20:29:53 +0000

    detect end msg

--- trunk/tools/logstash-filter-qssign/lib/logstash/filters/qssign.rb
+++ trunk/tools/logstash-filter-qssign/lib/logstash/filters/qssign.rb
@@ -4,21 +4,38 @@ require "logstash/namespace"
 
 #require 'digest'
 
-# This qssign filter will replace the contents of the default 
-# message field with whatever you specify in the configuration.
 #
-# It is only intended to be used as an qssign.
+# This "qssign" filter validates the signature and sequence number
+# of log messages and sets the "signature" field accordingly.
+#
+# You may use http://opensource.adnovum.ch/mod_qos/qssign.1.html
+# to sign your log data before storing/transferring it.
+#
+# See http://opensource.adnovum.ch/mod_qos/ for further
+# details.
+#
+# Copyright (C) 2015 Pascal Buchbinder
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is released under the GPL with the additional
+# exemption that compiling, linking, and/or using OpenSSL is allowed.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
 class LogStash::Filters::Qssign < LogStash::Filters::Base
 
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-  #
-  # filter {
-  #   qssign {
-  #     message => "My message..."
-  #   }
-  # }
-  #
   config_name "qssign"
   
   # Sample configuration:
@@ -45,18 +62,21 @@ class LogStash::Filters::Qssign < LogStash::Filters::Base
   #     }
   #   }
   # }
+
   config :message, :validate => :string, :default => "data"
   config :source, :validate => :string, :default => "path"
   config :sequence, :validate => :string, :default => "sequence"
   config :hmac, :validate => :string, :default => "hmac"
   config :secret, :validate => :string, :default => "12345"
 
-  #sources = Hash.new
+  def initialize(params)
+    super(params)
+    @sources = Hash.new
+  end
   
   public
   def register
-    # list of known input sources:
-    #sources = Hash.new
+
   end # def register
 
   public
@@ -90,21 +110,23 @@ class LogStash::Filters::Qssign < LogStash::Filters::Base
       end
     end
     if validHMAC == 1
-      seqenceNr = sequence.to_i
-      #expected = sources[source]
-      #if expected == nil
-      #  # first message from this source
-      #  event["signature"] = "valid"
-      #else
-      #  if expected == nextSequnce
-      #    event["signature"] = "valid"
-      #  else
-      #    event["signature"] = "wrong sequence"
-      #  end
-      #end
-      #nextSequence = sequence + 1
-      #sources[source] = nextSequence
-      event["signature"] = "valid"
+      sequenceNr = sequence.to_i
+      expected = @sources[source]
+      if expected == nil
+        # first message from this source
+        event["signature"] = "valid"
+      else
+        if expected == sequenceNr
+          event["signature"] = "valid"
+        else
+          event["signature"] = "wrong sequence"
+        end
+      end
+      nextSequence = sequenceNr + 1
+      @sources[source] = nextSequence
+      if data.end_with?(" qssign---end-of-data")
+        @sources[source] = 1
+      end
     else
       event["signature"] = "invalid"
     end
