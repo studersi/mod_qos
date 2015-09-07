56d2fe5b19caca97b29547afd448f5efba2a1416
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2015-09-07 19:36:16 +0000

    snapshot
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1881 49bada20-169a-462c-a56a-03074c89f563

--- tools/logstash-filter-qssign/lib/logstash/filters/qssign.rb
+++ tools/logstash-filter-qssign/lib/logstash/filters/qssign.rb
@@ -2,6 +2,8 @@
 require "logstash/filters/base"
 require "logstash/namespace"
 
+#require 'digest'
+
 # This qssign filter will replace the contents of the default 
 # message field with whatever you specify in the configuration.
 #
@@ -19,25 +21,94 @@ class LogStash::Filters::Qssign < LogStash::Filters::Base
   #
   config_name "qssign"
   
-  # Replace the message with this value.
-  config :message, :validate => :string, :default => "Hello World!"
-  
+  # Sample configuration:
+  #
+  # filter {
+  #   grok {
+  #     match => [ "message", "%{GREEDYDATA:data} %{INT:sequence}#%{NOTSPACE:hmac}" ]
+  #   }
+  #   if [data] {
+  #     qssign {
+  #       message => "data"
+  #       source => "path"
+  #       sequence => "sequence"
+  #       hmac => "hmac"
+  #       secret => "password"
+  #     }
+  #     mutate {
+  #       replace => [ "message", "%{data}" ]
+  #       remove_field => [ "data" ]
+  #     }
+  #   } else {
+  #     mutate {
+  #       add_field => [ "signature", "missing" ]
+  #     }
+  #   }
+  # }
+  config :message, :validate => :string, :default => "data"
+  config :source, :validate => :string, :default => "path"
+  config :sequence, :validate => :string, :default => "sequence"
+  config :hmac, :validate => :string, :default => "hmac"
+  config :secret, :validate => :string, :default => "12345"
 
+  #sources = Hash.new
+  
   public
   def register
-    # Add instance variables 
+    # list of known input sources:
+    #sources = Hash.new
   end # def register
 
   public
   def filter(event)
+    validHMAC = 0
 
-    if @message
-      # Replace the event message with our message as configured in the
-      # config file.
-      event["message"] = @message
+    # read the event fields...
+    # path of the file:
+    source = event[@source]
+    # sequence:
+    sequence = event[@sequence]
+    # hmac:
+    hmac = event[@hmac]
+    # data:
+    data = event[@message]
+
+    # configured shared secret:
+    key = @secret
+
+    # raw message:
+    raw = data + " " + sequence
+
+    # calculate hmac
+    digest = OpenSSL::Digest.new('sha1')
+    hmacin = OpenSSL::HMAC.digest(digest, key, raw)
+    encodedHmacin = Base64.strict_encode64(hmacin)
+
+    if (encodedHmacin <=> hmac) == 0
+      if encodedHmacin.length > 0
+        validHMAC = 1
+      end
     end
-
-    # filter_matched should go in the last line of our successful code
+    if validHMAC == 1
+      seqenceNr = sequence.to_i
+      #expected = sources[source]
+      #if expected == nil
+      #  # first message from this source
+      #  event["signature"] = "valid"
+      #else
+      #  if expected == nextSequnce
+      #    event["signature"] = "valid"
+      #  else
+      #    event["signature"] = "wrong sequence"
+      #  end
+      #end
+      #nextSequence = sequence + 1
+      #sources[source] = nextSequence
+      event["signature"] = "valid"
+    else
+      event["signature"] = "invalid"
+    end
+    
     filter_matched(event)
   end # def filter
 end # class LogStash::Filters::Qssign
