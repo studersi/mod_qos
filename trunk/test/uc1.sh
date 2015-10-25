898858b7e65a6ea4d874ccb612fb24dcfb610791
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2015-10-25 19:10:30 +0000

    simple QS_Status test

--- trunk/test/uc1.sh
+++ trunk/test/uc1.sh
@@ -366,6 +366,13 @@ sleep 1
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1status 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_Status.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+sleep 1
+
 if [ $ERRORS -ne 0 ]; then
   echo "$PFX test failed with $ERRORS errors"
   exit $ERRORS
