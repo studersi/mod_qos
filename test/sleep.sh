76503ac34dc7a9bc4d6765331764ca349f3a7b92
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2015-11-10 21:00:25 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@1929 49bada20-169a-462c-a56a-03074c89f563

--- test/sleep.sh
+++ test/sleep.sh
@@ -3,6 +3,11 @@
 S=${1:-60}
 SEC=`date '+%S'`
 SEC=`expr $S - $SEC`
+if [ $SEC -lt 0 ]; then
+  S=`expr $S + 60`
+  SEC=`date '+%S'`
+  SEC=`expr $S - $SEC`
+fi
 echo "sleep $SEC seconds ..."
 sleep $SEC
 
