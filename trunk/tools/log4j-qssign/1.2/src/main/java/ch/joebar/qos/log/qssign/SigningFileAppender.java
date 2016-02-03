d054b90f8c520261ce306bc31c86c26a9d240a44
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2016-02-03 21:10:12 +0000

    inital version

--- trunk/tools/log4j-qssign/1.2/src/main/java/ch/joebar/qos/log/qssign/SigningFileAppender.java
+++ trunk/tools/log4j-qssign/1.2/src/main/java/ch/joebar/qos/log/qssign/SigningFileAppender.java
@@ -1,3 +1,33 @@
+/*
+ * Utilities for the quality of service module mod_qos.
+ *
+ * Log data signing tool to ensure data integrity.
+ *
+ * See http://opensource.adnovum.ch/mod_qos/ for further
+ * details.
+ *
+ * Copyright (C) 2016 Pascal Buchbinder
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is released under the GPL with the additional
+ * exemption that compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
 package ch.joebar.qos.log.qssign;
 
 import org.apache.log4j.RollingFileAppender;
@@ -32,7 +62,8 @@ public class SigningFileAppender extends RollingFileAppender {
 	String enc = getEncoding();
 	if(enc != null) {
 	    try {
-		retval = new SigningOutputStreamWriter(os, enc);
+		Hmac h = new Hmac(secret, enc);
+		retval = new SigningOutputStreamWriter(os, enc, h);
 	    } catch(IOException e) {
 		if (e instanceof InterruptedIOException) {
 		    Thread.currentThread().interrupt();
@@ -42,7 +73,8 @@ public class SigningFileAppender extends RollingFileAppender {
 	    }
 	}
 	if(retval == null) {
-	    retval = new OutputStreamWriter(os);
+	    Hmac h = new Hmac(secret);
+	    retval = new SigningOutputStreamWriter(os, h);
 	}
 	return retval;
     }
