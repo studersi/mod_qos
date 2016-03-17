5b67ccebf7b0fbaafb7485068568367f3f6f2e95
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2016-03-17 16:30:37 +0000

    2-level client limit (rate per sec, violations per 5 minutes)

--- trunk/test24/ucn.sh
+++ trunk/test24/ucn.sh
@@ -44,6 +44,11 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D limit2level 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -seT scripts/UCN_limit2level.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
 
 if [ $ERRORS -ne 0 ]; then
   echo "$PFX test failed with $ERRORS errors"
