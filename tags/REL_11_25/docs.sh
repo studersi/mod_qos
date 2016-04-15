6c58679c42ebe5f2e12fa61a64af23c7d0510606
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2016-04-15 18:08:30 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_11_25'.

---
+++ tags/REL_11_25/docs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# -*-mode: ksh; ksh-indent: 2; -*-
+
+cd `dirname $0`
+
+mkdir -p doc/tmp
+
+VERSION=`grep "char g_revision" httpd_src/modules/qos/mod_qos.c | awk '{print $6}' | awk -F'"' '{print $2}'`
+
+A_V=`./httpd/httpd -v | grep "Server version" | awk -F'/' '{print $2}' | awk -F'.' '{print $1 "." $2}'`
+if [ "$A_V" != "2.2" ]; then
+    echo "FAILED, need Apache 2.2 binary to read log messages"
+    exit 1
+fi
+strings ./httpd/modules/qos/.libs/mod_qos.so | grep "mod_qos(" | sort -u | sort -n | grep -v -e "mod_qos()" -e "mod_qos(000)" > doc/MESSAGES.txt
+
+sed <doc/index.html >doc/tmp/index.html \
+    -e "s:DDoS:<a href='dos.html'>DDoS</a>:g" \
+    -e "s/0\.00/${VERSION}/g" \
+    -e "s:BUILD START -->::g" \
+    -e "s:<!-- BUILD END::g"
+
