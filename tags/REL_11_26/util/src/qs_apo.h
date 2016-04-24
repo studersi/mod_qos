589427c0a0d5c04f821a9ec5cd53e2d2c980491e
nobody <nobody@49bada20-169a-462c-a56a-03074c89f563>
2016-04-24 19:06:31 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_11_26'.

---
+++ tags/REL_11_26/util/src/qs_apo.h
@@ -0,0 +1,31 @@
+/**
+ * Utilities for the quality of service module mod_qos.
+ *
+ * See http://opensource.adnovum.ch/mod_qos/ for further
+ * details.
+ *
+ * Copyright (C) 2007-2015 Pascal Buchbinder
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
+#ifndef QS_APO_H
+#define QS_APO_H
+
+char *qs_readpwd(apr_pool_t *pool, const char *prg);
+
+#endif
