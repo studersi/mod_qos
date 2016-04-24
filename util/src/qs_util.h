644059cca9d8455ca9099436169fc1deb26068af
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2016-04-24 19:06:30 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@2082 49bada20-169a-462c-a56a-03074c89f563

--- util/src/qs_util.h
+++ util/src/qs_util.h
@@ -29,7 +29,7 @@
 /* ----------------------------------
  * version info
  * ---------------------------------- */
-static const char man_version[] = "11.25";
+static const char man_version[] = "11.26";
 static const char man_date[] = "April 2016";
 
 /* ----------------------------------
