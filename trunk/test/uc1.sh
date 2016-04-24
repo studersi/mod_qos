b32506e0653815b2aa5223b1a70ff11f1b74a396
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2016-04-24 19:56:57 +0000

    net

--- trunk/test/uc1.sh
+++ trunk/test/uc1.sh
@@ -354,6 +354,12 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D net 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_net.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 ../httpd/httpd -d `pwd` -f conf/uc1.conf -D v6 -D uc1w 2>/dev/null 1>/dev/null
 waitApache
 ./run.sh -s scripts/UC1_QS_ClientSerialize_v6.htt
