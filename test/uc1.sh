4c92987c0f0655e95cdb3746b5ba75409370e111
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2016-11-16 21:31:28 +0000

    v event
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@2184 49bada20-169a-462c-a56a-03074c89f563

--- test/uc1.sh
+++ test/uc1.sh
@@ -379,6 +379,12 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1z 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -s scripts/UC1_QS_VipIPUser.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 ../httpd/httpd -d `pwd` -f conf/uc1.conf -D uc1KK 2>/dev/null 1>/dev/null
 waitApache
 ./run.sh -s scripts/UC1_QS_MaxKeepAliveRequests.htt
