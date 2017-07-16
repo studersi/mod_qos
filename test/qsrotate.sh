18f6cfb2e564ad3a944dfe92473b193a96c8df69
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2017-07-16 20:41:05 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@2260 49bada20-169a-462c-a56a-03074c89f563

--- test/qsrotate.sh
+++ test/qsrotate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: qsrotate.sh,v 2.6 2017-07-14 14:36:16 pbuchbinder Exp $
+# $Id: qsrotate.sh,v 2.7 2017-07-16 20:41:05 pbuchbinder Exp $
 
 cd `dirname $0`
 PFX=[`basename $0`]
@@ -64,7 +64,7 @@ done
 
 # 4 SIGUST1
 rm -f usr1.log*
-for E in `seq 3`; do date; sleep 1; done | ../util/src/qsrotate -o usr1.log &
+for E in `seq 3`; do date +'%s'; sleep 1; done | ../util/src/qsrotate -o usr1.log &
 qpid=`ps -ef | grep usr1.log | grep qsrotate | awk '{print $2}'`
 sleep 1
 kill -USR1 $qpid
@@ -74,8 +74,14 @@ if [ `ls -l usr1.log* | wc -l` -ne 2 ]; then
     ls -l usr1.log*
     ERROR=1
 fi
+last=`tail -1 usr1.log.2*`
+next=`echo $last+1|bc`
+if [ "`head -1 usr1.log`" != "$next" ]; then
+    echo "$PFX FAILED, USR1 $next"
+    ls -l usr1.log*
+    ERROR=1
+fi
 rm -f usr1.log*
-
 
 if [ $ERROR -ne 0 ]; then
     exit 1
