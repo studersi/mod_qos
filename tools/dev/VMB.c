dbda4cf085c073b08377cd1ea827441712e0b07e
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2018-03-13 19:43:07 +0000

    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@2418 49bada20-169a-462c-a56a-03074c89f563

--- tools/dev/VMB.c
+++ tools/dev/VMB.c
@@ -3,21 +3,32 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/time.h>
+#include <math.h>
 
 #define MAX 2
 #define MSIZE 1048576
 
+static const char revision[] = "$Revision$";
+
 int main(int argc, const char *const argv[]) {
+  char hx;
   int i, k;
   int maxSize = MSIZE;
   int inx = 0;
-  long counter = 0;
+  long long counter = 0;
   char *ar[MAX];
-  struct timeval tv;
-  long long start, end, memory, cpu;
-  char buff[] = "abcdefghABCDEFG123\n";
+  double d;
+  
+  char buff[] = "abcdefghABCDEFGXXXXXXXXXXXX123\n";
   char *p;
+
+  char hostname[1024];
+  char timeBuff[64];
+  struct timeval tv;
+  struct tm* tm_info;
+  long long start, end, memory, cpu;
   
   argv++;
   argc--;
@@ -30,9 +41,16 @@ int main(int argc, const char *const argv[]) {
     maxSize = MSIZE;
   }
 
+  printf("========================\n");
+  gettimeofday(&tv, NULL);
+  tm_info = localtime(&tv.tv_sec);
+  strftime(timeBuff, 26, "%Y:%m:%d %H:%M:%S", tm_info);
+  gethostname(hostname, 1023);
+  printf("VMB@%s %s %s\n", hostname, timeBuff, revision);
+
+  // alloc and write memory
   printf("memory alloc/write %dx%d bytes (%dMB) ",
 	 MAX, maxSize, (MAX*maxSize)/1024/1024);
-
   gettimeofday(&tv, NULL);
   start = tv.tv_sec * 1000000 + tv.tv_usec;
   for(inx = 0; inx<MAX; inx++) {
@@ -62,10 +80,11 @@ int main(int argc, const char *const argv[]) {
   end = tv.tv_sec * 1000000 + tv.tv_usec;
   memory = (end - start)/1000;
 
+  // just some bogus operations...
   printf("generic operations ");
   start = tv.tv_sec * 1000000 + tv.tv_usec;
   gettimeofday(&tv, NULL);
-  for(i = 0; i < 10000000; i++) {
+  for(i = 1; i < 10000000; i++) {
     counter++;
     p = strchr(buff, '1');
     k = atoi(p);
@@ -74,17 +93,39 @@ int main(int argc, const char *const argv[]) {
       fflush(stdout);
       printf(".");
     }
+    if(counter > 1000000) {
+      counter = counter - 100000;
+    } else {
+      counter = counter * 2;
+    }
+    for(k = 1; k <15; k++) {
+      int t = (k%i) & counter;
+      counter = counter - t;
+      counter = counter / k;
+    }
+    for(k = 1; k <15; k++) {
+      hx = (counter + k) & 0xff;
+      hx = hx >> 1;
+      counter = counter + hx;
+    }
+    d = 1;
+    for(k = 1; k <15; k++) {
+      d = d + 0.2;
+      if((d * 10 / 10) == d) {
+	d = 1;
+      }
+    }
   }
-  printf("\n");
+
   gettimeofday(&tv, NULL);
+  printf("%lld\n", counter+100);
   end = tv.tv_sec * 1000000 + tv.tv_usec;
   cpu = (end - start)/1000;
   
-  printf("==========================\n");
-  printf("memory: %lldms\n", memory);
-  printf("processing: %lldms\n", cpu);
-  printf("io: n/a\n");
-  printf("==========================\n");
+  printf("========================\n");
+  printf("memory:     %10lldms\n", memory);
+  printf("processing: %10lldms\n", cpu);
+  printf("========================\n");
 	 
   return 0;
 }
