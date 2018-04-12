25ac6a8896d3ee10ad701a771e1cba9524145e54
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2018-04-12 19:30:46 +0000

    doc qsloc use case

--- trunk/doc/dist/sync.sh
+++ trunk/doc/dist/sync.sh
@@ -25,6 +25,9 @@ sleep $T
 #scp ../images/Serialization.png ${dest}/images/
 #scp ../images/Events.png ${dest}/images/
 #scp ../images/Rule.png ${dest}/images/
+#scp ../images/qsloc1.png ${dest}/images/
+#scp ../images/qsloc2.png ${dest}/images/
+#scp ../images/qsloc3.png ${dest}/images/
 scp ../images/*.png ${dest}/images/
 scp ../images/mod_qos.gif ${dest}/images/
 scp ../qsfilter2_process.gif ${dest}/
