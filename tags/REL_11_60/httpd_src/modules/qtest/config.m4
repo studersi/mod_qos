e6cdb8bf8034b2b977611e9c6e5a95bc31273c9b
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2018-12-08 19:23:30 +0000

    11.60, back to orignial code

--- tags/REL_11_60/httpd_src/modules/qtest/config.m4
+++
@@ -1,6 +0,0 @@
-
-APACHE_MODPATH_INIT(qtest)
-
-APACHE_MODULE(qtest, qos test module, , , shared)
-
-APACHE_MODPATH_FINISH
--- tags/REL_11_60/test/kill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-p=`cat logs/apache.pid`
-for E in `ps -ef | grep httpd | grep $p | awk '{print $2}' | grep -v $p`; do
-  echo $E
-  kill -11 $E
-done
-
