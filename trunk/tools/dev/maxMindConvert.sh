d8b22cb29b112229fb8316ac83951611947d4da2
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2019-06-13 05:27:50 +0000

    doc

--- trunk/tools/dev/maxMindConvert.sh
+++ trunk/tools/dev/maxMindConvert.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 # -*-mode: ksh; ksh-indent: 2; -*-
+#
+# Converts GeoLite2 Country CSV files into the a format
+# which can be loaded by mod_qos (similar to GeoLite Legacy).
+#
+# The GeoLite2-Country-CSV_<date>.zip archive usually contains
+# the IP range/block defintion file "GeoLite2-Country-Blocks-IPv4.csv"
+# as well as the ISO 3166 country code block mapping file
+# "GeoLite2-Country-Locations-en.csv".
+#
+# The script uses the mod_qos binaries net2range and qsgeo.
+#
 
 block=GeoLite2-Country-Blocks-IPv4.csv
 list=GeoLite2-Country-Locations-en.csv
+net2range=./net2range
+qsgeo=../../util/src/qsgeo
+
 rm -f tmp.txt
 for E in `grep -v geoname_id $list | awk -F',' '{print $1 "," $3 "," $5}'`; do
   blockid=`echo $E | awk -F',' '{print $1}'`
@@ -11,12 +25,12 @@ for E in `grep -v geoname_id $list | awk -F',' '{print $1 "," $3 "," $5}'`; do
       country=`echo $E | awk -F',' '{print $2}'`
   fi
   for net in `grep ",$blockid," $block | awk -F',' '{print $1}'`; do
-      range=`./net2range $net`
+      range=`${net2range} $net`
       netstart=`echo $range | awk -F',' '{print $3}'`
       netend=`echo $range | awk -F',' '{print $4}'`
       echo "$netstart,$netend,\"$country\"" >> tmp.txt
   done
 done
 cat tmp.txt | sort -n > tmp.sorted.txt
-../../util/src/qsgeo -d tmp.sorted.txt -l
+${qsgeo} -d tmp.sorted.txt -l
 
