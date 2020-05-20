23b72268c912edafd202423e699bcf6e2f5f7227
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2020-05-20 20:41:29 +0000

    QS_ClientIpFromHeader supporting pseudo IP

--- trunk/test24/ucn.sh
+++ trunk/test24/ucn.sh
@@ -123,6 +123,12 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D BASICAUTH 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -seT scripts/UCN_QS_ClientIpFromHeader_basicAuth.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 if [ $ERRORS -ne 0 ]; then
   echo "$PFX test failed with $ERRORS errors"
   exit $ERRORS
