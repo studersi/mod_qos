e44caf9d2e0877b64436181f6de3ea9eaf2bd2be
pbuchbinder <pbuchbinder@49bada20-169a-462c-a56a-03074c89f563>
2021-09-24 16:37:16 +0000

    UCN_QS_ClientGeoCountryPriv.htt
    git-svn-id: https://svn.code.sf.net/p/mod-qos/source/trunk@2629 49bada20-169a-462c-a56a-03074c89f563

--- test24/ucn.sh
+++ test24/ucn.sh
@@ -135,6 +135,12 @@ waitApache
 ERRORS=`expr $ERRORS + $?`
 ./ctl.sh stop 2>/dev/null 1>/dev/null
 
+../httpd/httpd -d `pwd` -f conf/ucn.conf -D QS_ClientGeoCountryPriv 2>/dev/null 1>/dev/null
+waitApache
+./run.sh -seT scripts/UCN_QS_ClientGeoCountryPriv.htt
+ERRORS=`expr $ERRORS + $?`
+./ctl.sh stop 2>/dev/null 1>/dev/null
+
 
 
 if [ $ERRORS -ne 0 ]; then
